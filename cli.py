5d68d46364d40cc2d5d9b4a9c4cf1a66317de05c
Eduards Zuga <zuga.eduards@gmail.com>
2025-03-28 17:46:01 +0200

    mazi skaidrojumi kodam

--- cli.py
+++ cli.py
@@ -2,10 +2,10 @@ from gamestate import GameState
 from tree import GameStateTree
 from algo import minimax, alphabeta
 
+# Sākotnējā spēles inicializācija (izsaukta, kad sāk spēli no jauna)
 def initialize_game(game_length, ai_start):
     game = GameState(ai_start, game_length)
     game_tree = GameStateTree()
-    game_tree.create_tree(game)
     print(f"Spēles koks ar dziļumu: {game.dynamic_depth()}")
 
     return game, game_tree
@@ -17,26 +17,29 @@ if __name__ == "__main__":
         algo_input = int(input("Izvēlieties algoritmu (1 - Minimax, 2 - Alpha-Beta): "))
         algo = minimax if algo_input == 1 else alphabeta
 
-
+        # Inicializē spēli
         game, game_tree = initialize_game(game_length, ai_start)
         print(f"Spēli sāk { 'Dators' if ai_start else 'Spēlētājs' }")
         print("-----------------")
 
+        # Spēles cikls, kamēr spēle nav beigusies
         while not game.game_over():
             game.print_state()
             move_list = game.available_moves()
             print(f"Gājienu izvēle: {move_list}")
 
-            # ChatGPT kods
             scores = []
+            # ChatGPT kods // Izdrukā gājienu vērtējumus
             # Evaluate each available move's score from the current state
             for idx, move in enumerate(game.available_moves(), start=1):
                 cloned_state = game.clone_state()  # Clone the current state
                 if cloned_state.make_move(idx, cloned_state.ai_turn):
                     eval_result = cloned_state.evaluate()  # (score, point_diff, move_score)
                     scores.append(round(eval_result, 2))
-            
+            # // ChatGPT kods beidzas
             print(f"Gājienu vērtējumi: {scores}")
+
+            # Katrā datora gājienā pārtaisa spēles koku
             if game.ai_turn:
                 game_tree.create_tree(game)
                 _, move = algo(game_tree.root)
@@ -44,6 +47,7 @@ if __name__ == "__main__":
             else:
                 move = int(input("Ievadiet gājiena numuru: "))
 
+            # Pārbauda vai gājiens ir izdarāms un izdara to
             if not game.make_move(move, game.ai_turn) :
                 print("Nederīgs gājiens")
                 continue
